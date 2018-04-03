60934902787dae4bcbbbfc32ca1b5fae87271318
Simone Zugarini <zuga_simo@hotmail.it>
2018-04-03 14:52:13 +0200

    Creazione Unit Test

---
+++ TestCollezioni/CollezioneLibrary.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CollezioneLibrary.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CollezioneLibrary.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5b65c83a-83f9-49b2-aca7-5610b65b2e2d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestCollezioni/CollezioneLibrary.Test/VettoreTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CollezioneLibrary.Test
+{
+    [TestClass]
+    public class VettoreTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
